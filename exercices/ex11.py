026ec56402894341c4620379de79cc210d22b95a
matheus <matheusgodoy@me.com>
2014-02-06 07:29:35 -0200

    add breackpoint test

--- exercices/ex11.py
+++ exercices/ex11.py
@@ -1,10 +1,10 @@
 # http://learnpythonthehardway.org/book/ex11.html
 
-print "How old are you?",
+print ("How old are you?"),
 age = raw_input()
-print "How tall are you?",
+print ("How tall are you?"),
 height = raw_input()
-print "How much do you weight?",
+print ("How much do you weight?"),
 weight = raw_input()
 
-print "So, you're %r old, %r tall and %r heavy." % (age, height, weight)
+print ("So, you're %r old, %r tall and %r heavy.") % (age, height, weight)
